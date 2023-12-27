d50434674011a93302edf60bcb916a399231802a
Eddington <eampofu@gmail.com>
2023-12-27 20:58:40 +0200

    updated showquestions function to display the questions and answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,13 +10,33 @@ var feedbackDiv=document.getElementById("feedback");
 
 var endScreenDiv=document.getElementById("end-screen");
 
+var Index=7;
+var currentQuestion=questions[Index];
+
 var timeLeft =60;
 
 function showQuestions(){
+    //console.log(currentQuestion.question.toString());
+questionTitle.textContent=currentQuestion.question ;//"add question here";
+console.log(questionOptionsDiv.length)
+for (let i = 0; i < 4; i++) {
+   
 
+    var btn = document.createElement("button");
+    btn.textContent=currentQuestion.answers[i]
+    console.log("btnelement"+btn);
+    btn.setAttribute("id",i)
+    //btn.addEventListener('click',"act")
+    questionOptionsDiv.appendChild(btn);
+    
+}
 };
 
 function startGame(){
+    var showhide='hide';
+    startScreenDiv.setAttribute("class",showhide);
+    questionsDiv.removeAttribute("class", showhide);
+    showQuestions();
 timerStart();
 };
 
