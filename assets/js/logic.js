f7f55f0837355b382355ed4efa7ef8fa98f4fa1d
Eddington <eampofu@gmail.com>
2023-12-28 20:47:16 +0200

    added functionality for ending game when time runs out or the quiz is complete.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,7 @@
 var clock = document.getElementById("time");
 var startScreenDiv = document.getElementById("start-screen");
 var startBtn = document.getElementById("start");
+var saveBtn=document.getElementById("submit")
 //quiz related
 var questionsDiv = document.getElementById("questions");
 var questionTitle = document.getElementById("question-title");
@@ -13,8 +14,9 @@ var message = document.createElement("h4");
 var hrule=document.createElement("hr");
 feedbackDiv.appendChild(hrule)
 feedbackDiv.appendChild(message);
+var finalscore=document.getElementById("final-score");
 var questionIndex = 0;
- 
+ var score=0;
 
 var timeLeft = 60;
 // function used to displaythe question and its responses
@@ -23,25 +25,25 @@ function showQuestions() {
   console.log("the next "+questionIndex);
  
   questionTitle.textContent = quiz[questionIndex].question; //"add question here";
-  console.log("letngh  "+quiz.length);
-  for (let i = 0; i < 4; i++) {
+  console.log("letngh  "+quiz[questionIndex].answers.length);
+  for (let i = 0; i < quiz[questionIndex].answers.length; i++) {
    
     var btn = document.createElement("button");
     btn.textContent = quiz[questionIndex].answers[i];
-    console.log("btnelement" + btn);
-    btn.setAttribute("id", i);
+   
+    btn.setAttribute("data-answer", i);
     btn.addEventListener("click", btnListener);
     questionOptionsDiv.appendChild(btn);
-    console.log("no nodes")
+   
 
 
   }
 }
 // function to start the quiz when the start button is clicked
 function startGame() {
-  var showhide = "hide";
-  startScreenDiv.setAttribute("class", showhide);
-  questionsDiv.removeAttribute("class", showhide);
+ 
+  startScreenDiv.setAttribute("class",  "hide");
+  questionsDiv.removeAttribute("class",  "hide");
   showQuestions();
   timerStart();
 }
@@ -51,37 +53,66 @@ function timerStart() {
     timeLeft--;
 
     clock.textContent = timeLeft;
-    if (timeLeft == 0) {
+    if (timeLeft === 0 || timeLeft < 0||questionIndex===quiz.length-1) {
+    
+      gameOver();
       clearInterval(countdown);
     }
   }, 1000);
 }
 
+
 //function for when one of the answers are clicked
 function btnListener(event) {
-  console.log(event.target.id);
+   const element=event.target;
 
-  if (quiz[questionIndex].answer == event.target.id) {
-   // alert("welldone");
-
+  if (quiz[questionIndex].answer == element.dataset.answer) {
+  
+score++
     message.textContent = "Correct!";
+    var correctSound = new Audio("./assets/sfx/correct.wav");
+    correctSound.play();
+    hideFeedback = setTimeout(function () {
+        feedbackDiv.classList.add("hide");
+        clearTimeout(hideFeedback);
+      }, 2800);
   } else {
     message.textContent = "Incorrect !";
-
+    var wrongSound = new Audio("./assets/sfx/incorrect.wav");
+    wrongSound.play();
+    hideFeedback = setTimeout(function () {
+        feedbackDiv.classList.add("hide");
+        clearTimeout(hideFeedback);
+      }, 2800);
+timeLeft-=10;
   }
-  feedbackDiv.removeAttribute("class", "hide");
+ feedbackDiv.removeAttribute("class", "hide");
   displayNextQuestion();
+  console.log("scores "+score)
 }
 //function to move to the next question
 function displayNextQuestion(){
-    if(questionIndex<quiz.length){
+    if(questionIndex!=quiz.length-1){
         questionIndex++;
 console.log("from showq");
-
 questionOptionsDiv.innerHTML='';
 showQuestions();
-//feedbackDiv.setAttribute("class", "hide");
+
     }
+    
+}
+//function to end the game 
+function gameOver(){
+ finalscore.textContent   =score;
+    questionsDiv.setAttribute("class","hide");
+    endScreenDiv.removeAttribute("class","hide");
+    
+}
+saveScores=function(event){
+const element =event.target;
+event.preventDefault();
+
 }
 
 startBtn.addEventListener("click", startGame);
+saveBtn.addEventListener("click",saveScores);
